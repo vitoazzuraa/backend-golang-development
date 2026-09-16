75247be01003832d7f76bdd16db0f2c1bbefc2c4
vitoazzuraa <christianusvito13@gmail.com>
2026-09-16 20:24:44 +0700

    helper jwt dan context

---
+++ api-student/helper/context.go
@@ -0,0 +1,14 @@
+package helper
+
+import (
+	"backend-go/api-student/app/model"
+	"github.com/gofiber/fiber/v2"
+)
+
+const LocalsAuthUser = "authUser"
+
+func CurrentUser(c *fiber.Ctx) (model.AuthUser, bool) {
+	user, ok := c.Locals(LocalsAuthUser).(model.AuthUser)
+
+	return user, ok
+}
